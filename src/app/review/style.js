41355d034eeb5edf2051234eb025b629c91257cf
kimgyuha <kimgyuha25@gmail.com>
2023-07-05 08:51:23 +0900

    ADD : Review 페이지

---
+++ src/app/review/style.js
@@ -0,0 +1,18 @@
+import styled from "@emotion/styled";
+
+export const FlexBox = styled.div`
+    display: flex;
+    flex-direction: column;
+`
+
+export const FlexBox2 = styled.div`
+    display: flex;
+    justify-content: center;
+`
+export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 50px;
+    width: 90%;
+    margin-top: 50px;
+`
