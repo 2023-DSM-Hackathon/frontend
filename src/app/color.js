b46a23ea36f5269c468c16bb54ff99ccec2421a7
kimgyuha <kimgyuha25@gmail.com>
2023-07-04 19:28:47 +0900

    Setting : color 세팅

---
+++ src/app/color.js
@@ -0,0 +1,10 @@
+export const color = {
+    primary1:"#E1FCCF",
+    primary2:"#BDFA9D",
+    primary3:"#8DF36C",
+    primary4:"#62E547",
+    primary5:"#23D412",
+    primary6:"#0CB60D",
+    primary7:"#089814",
+    primary8:"#067A17"
+};
