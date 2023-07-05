7b4b45ac07c728431264cd7db58d5adbf5ffe1c3
kimgyuha <kimgyuha25@gmail.com>
2023-07-05 15:12:38 +0900

    ADD : 마이페이지 연동

--- src/app/mypage/page.js
+++ src/app/mypage/page.js
@@ -1,44 +1,56 @@
 "use client"
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Header from "../components/common/Header";
 import User from "../components/pages/mypage/user";
 import * as S from "./style";
 import Post from "../components/pages/mypage/post";
+import axios from "axios";
+
+const BASEURL = process.env.NEXT_PUBLIC_SERVER;
+
 
 const Mypage = () =>{
-    const [posts, setPosts] = useState([
-        {
-            title:"내가 모집한 줍깅",
-            post: [{title:"줍깅하실분", date:"2023.07.04", time:"23:00", place:"asdasdasasdasdasasdasdasasdasdas", name:"홍길동"},{title:"asdas", date:"123.213.2", time:"23:00", place:"asdasdas", name:"asdas"},{title:"asdas", date:"123.213.2", time:"23:00", place:"asdasdas", name:"asdas"},{title:"asdas", date:"123.213.2", time:"23:00", place:"asdasdas", name:"asdas"},{title:"asdas", date:"123.213.2", time:"23:00", place:"asdasdas", name:"asdas"}],
-            link:"/",
-            type:1
-        },{
-            title:"참가 신청한 줍깅",
-            post: [{title:"asdas",content:"asdasdasdasdfasdfasdfasdasdasdasdfasdfasdfasdasdasdasdfasdfasdfasdasdasdasdfasdfasdfasdasdasdasdfasdfasdfasdasdasdasdfasdfasdfasdasdasdasdfasdfasdfasdasdasdasdfasdfasdfasdasdasdasdfasdfasdfasdasdasdasdfasdfasdfasdasdasdasdfasdfasdfasdasdasdasdfasdfasdfasdasdasdasdfasdfasdfasdasdasdasdfasdfasdfasdasdasdasdfasdfasdfasdasdasdasdfasdfasdfasdasdasdasdfasdfasdfasdasdasdasdfasdfasdfasdasdasdasdfasdfasdfasdasdasdasdfasdfasdfasdasdasdasdfasdfasdfasdasdasdasdfasdfasdf", name:"asdas"},{title:"asdas",content:"asdasdasdasdfasdfasdf", img:"assets/authBgImg.png", name:"asdas"},{title:"asdas",content:"asdasdasdasdfasdfasdf", img:"assets/AppLogo.png", name:"asdas"},{title:"asdas",content:"asdasdasdasdfasdfasdf", img:"assets/AppLogo.png", name:"asdas"}],
-            link:"/",
-            type:1
-        },{
-            title:"내가 작성한 후기글",
-            post: [{title:"asdas",content:"asdasdasdasdfasdfasdfasdasdasdasdfasdfasdfasdasdasdasdfasdfasdfasdasdasdasdfasdfasdf", img:"assets/AppLogo.png", name:"asdas"},{title:"asdas",content:"asdasdasdasdfasdfasdf", img:"assets/AppLogo.png", name:"asdas"},{title:"asdas",content:"asdasdasdasdfasdfasdf", img:"assets/AppLogo.png",date:"123.213.2", time:"23:00", place:"asdasdas", name:"asdas"}],
-            link:"/",
-            type:2
-        }
-    ])
+    const [user, setUser] = useState();
+    const [acachievements,setAchievements] =useState([]);
+    const [feeds, setFeeds] = useState([]);
+    const [applies, setApplies] = useState([]);
+    const [reviews, setReviews] = useState([]);
+
+    useEffect(()=>{
+        const token = localStorage.getItem('token');
+
+        axios
+        .request({
+            url: `${BASEURL}/users`,
+            method: 'get',
+            headers: {
+                "Authorization": `Bearer ${token}`
+            }
+        })
+        .then((res) => {
+            const data = res.data
+            setUser(data.user_profile);
+            setAchievements(data.achievements);
+            setFeeds(data.feeds);
+            setFeeds(data.applies);
+            setFeeds(data.reviews);
+        })
+        .catch(() => {
+            alert('내 정보 불러오기 실패');
+        });
+    },[])
 
     return(
         <S.FlexBox>
             <Header/>
                 <S.FlexBox2>
                 <S.Container>
-                    <User/>
-                    {
-                        posts.map(d=>{
-                            return(
-                                <Post type={d.type} title={d.title} posts={d.post} link={d.link}/>
-                            )
-                        })
-                    }
+                    <User {...user} acachievements={acachievements}/>
+                    <Post type={1} posts={feeds} title="내가 모집한 줍깅" link="/myrecruit"  />
+                    <Post type={1} posts={applies} title="참가 신청한 줍깅" link="/book"  />
+                    <Post type={2} posts={reviews} title="내가 작성한 후기글" link="/myreview"  />
+                    
                 </S.Container>  
             </S.FlexBox2>
         </S.FlexBox>
