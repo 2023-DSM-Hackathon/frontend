85535a1c33a33f104a92914b150fbbda377ef315
kimgyuha <kimgyuha25@gmail.com>
2023-07-05 13:47:16 +0900

    ADD : main page

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,78 @@
 "use client"
+import { useState } from "react";
+import * as S from "./style";
+import Header from "./components/common/Header";
+import { Review } from "../../public/assets/review";
+import { useRouter, useSearchParams } from "next/navigation";
+import { color } from "./color";
+import RecruitmentLarge from "./components/common/RecruitmentLarge";
+import { Recruitment } from "../../public/assets/recruitment";
+import PostVeiw from "./components/common/postVeiw";
 
-export default function Home() {
-  return (<>asd</>)
+const Main = () => {
+  const param = useSearchParams();
+  const type = param.get('type');
+  const router = useRouter();
+
+  const [post, setPost] = useState([{
+    "id": 1,
+		"nickname": "닉네임",
+		"title": "제목",
+		"content": "내용",
+		"image_url": "이미지 url",
+		"comment_count": 3
+  }]);
+
+    return(
+        <S.FlexBox>
+            <Header/>
+                <S.FlexBox2>
+                <S.Container>
+                  <S.SelectContainer>
+                    {
+                    type === "re" ?
+                      <>
+                        <S.SelectButton color={color.grayBase} onClick={()=>router.push('/')}>
+                          <Recruitment fill={color.grayBase}/>
+                          <p>모집</p>
+                        </S.SelectButton>
+                        <S.SelectButton onClick={()=>router.push('/?type=re')}>
+                          <Review/>
+                          <p>후기</p>
+                        </S.SelectButton>
+                      </>
+                    :
+                      <>
+                        <S.SelectButton onClick={()=>router.push('/')}>
+                          <Recruitment fill="black"/>
+                          <p>모집</p>
+                        </S.SelectButton>
+                        <S.SelectButton color={color.grayBase} onClick={()=>router.push('/?type=re')}>
+                          <Review fill={color.grayBase}/>
+                          <p>후기</p>
+                        </S.SelectButton>
+                      </>
+                    }
+                  </S.SelectContainer>
+                  <S.PostContainer>
+                    {
+                      post.map(d=>{
+                        return(
+                          <>
+                            {type === "re"?
+                            <PostVeiw {...d}/>
+                            :
+                            <RecruitmentLarge {...d}/>
+                          }
+                          </>
+                        )
+                      })
+                    }
+                  </S.PostContainer>
+                </S.Container>  
+            </S.FlexBox2>
+        </S.FlexBox>
+    )
 }
+
+export default Main;
---
+++ src/app/style.js
@@ -0,0 +1,37 @@
+import styled from "@emotion/styled";
+
+export const FlexBox = styled.div`
+    display: flex;
+    flex-direction: column;
+`
+
+export const FlexBox2 = styled.div`
+    display: flex;
+    justify-content: center;
+`
+export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 30px;
+    width: 70%;
+    margin-top: 35px;
+`
+
+export const SelectContainer = styled.div`
+    display: flex;
+`
+export const SelectButton = styled.div`
+    display: flex;
+    gap: 10px;
+    padding: 3px 15px;
+    border-bottom: ${props => props.color ? "" : `3px solid black`};
+    >p {
+        color: ${props => props.color }
+    }
+`
+
+export const PostContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 15px;
+`
