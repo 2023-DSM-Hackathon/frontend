636db7624ff7f28f090c3d340fbcf5494466b42b
kimgyuha <kimgyuha25@gmail.com>
2023-07-05 09:37:02 +0900

    ADD : 모집글 작성 페이지

---
+++ src/app/recruitmentWrite/page.js
@@ -0,0 +1,63 @@
+"use client"
+
+import { useState } from "react";
+import Header from "../components/common/Header";
+import Input from "../components/common/Input";
+import * as S from "./style";
+
+const RecruitmentWrite = () =>{
+
+    const [userData, setUserData] = useState({
+		title: "",
+		date: "",
+        time:"",
+        place:"",
+        person:0,
+        content:""
+	});
+
+    const inputType = [
+        { name: "title", title: "제목", placeholder: "제목을 입력해주세요", type:"text"},
+		{ name: "date", title: "날짜", placeholder: "날짜를 입력해주세요" , type:"date"},
+		{ name: "time", title: "시간", placeholder: "시간을 입력해주세요" , type:"time"},
+		{ name: "place", title: "장소", placeholder: "장소를 입력해주세요 (자세히)" , type:"text"},
+		{ name: "person", title: "모집 인원수", placeholder: "모집 인원수를 입력해주세요" , type:"number"},
+		{ name: "content", title: "내용", placeholder: "내용을 입력해주세요" , type:"text"},
+	];
+
+    const onChange = (e) => {
+		const { name, value } = e.target;
+		setUserData({
+			...userData,
+			[name]: value
+		})
+    }
+
+    return(
+        <S.FlexBox>
+            <Header/>
+                <S.FlexBox2>
+                <S.Container>
+                    <S.InputContainer>
+                        <Input onChange={onChange} title={inputType[0].title} name={inputType[0].name} placeholder={inputType[0].placeholder} type={inputType[0].type}/>
+                        <S.SmallInput>
+                            <Input onChange={onChange} title={inputType[1].title} name={inputType[1].name} placeholder={inputType[1].placeholder} type={inputType[1].type}/>
+                            <Input onChange={onChange} title={inputType[2].title} name={inputType[2].name} placeholder={inputType[2].placeholder} type={inputType[2].type}/>
+                        </S.SmallInput>
+                        <S.SmallInput>
+                            <Input onChange={onChange} title={inputType[3].title} name={inputType[3].name} placeholder={inputType[3].placeholder} type={inputType[3].type}/>
+                            <Input onChange={onChange} title={inputType[4].title} name={inputType[4].name} placeholder={inputType[4].placeholder} type={inputType[4].type}/>
+                        </S.SmallInput>
+                        <S.TextAreaContainer>
+                            <S.InputTitle>{inputType[5].title}</S.InputTitle>
+                            <S.TextArea onChange={onChange} name={inputType[5].name} placeholder={inputType[5].placeholder}/>
+                        </S.TextAreaContainer>
+                    </S.InputContainer>
+                    <S.Submit>등록하기</S.Submit>
+                </S.Container>  
+            </S.FlexBox2>
+        </S.FlexBox>
+    )
+}
+
+export default RecruitmentWrite;
---
+++ src/app/recruitmentWrite/style.js
@@ -0,0 +1,69 @@
+import styled from "@emotion/styled";
+import { color } from "../color";
+
+export const FlexBox = styled.div`
+    display: flex;
+    flex-direction: column;
+`
+
+export const FlexBox2 = styled.div`
+    display: flex;
+    justify-content: center;
+`
+export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 90%;
+    margin-top: 50px;
+    margin-bottom: 100px;
+    gap: 30px;
+`
+
+export const InputContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 15px;
+`
+
+export const SmallInput = styled.div`
+    display: flex;
+    gap: 15px;
+`
+
+export const TextAreaContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+`
+export const InputTitle = styled.p`
+
+`
+export const TextArea = styled.textarea`
+    outline: none;
+    height: 300px;
+    padding: 12px 14px;
+    border-radius: 8px;
+    border: none;
+    font-size: 16px;
+    resize: vertical;
+
+    background-color: ${color.grayLight2};
+    &::placeholder{
+        color: ${color.grayBase};
+    }
+`
+
+export const Submit = styled.button`
+    border-radius: 8px;
+    background-color: ${color.primary6};
+    color: white;
+    font-size: 15px;
+    font-weight: bold;
+    padding: 14px 0px;
+    border: none;
+    :hover {
+		background-color: ${color.primary5};
+	}
+    :active {
+        background-color: ${color.primary8};
+    }
+`
